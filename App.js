f109972fca5d36635439b9b5f22eb2354eaab0e3
Pablo Lizardo <pablolizardo@mbp.local>
2020-08-20 15:07:21 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import Logo from './assets/icon.png'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Image source={ Logo }/>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder="Username" style={styles.textInput}/>
+      <TextInput placeholder="Password" style={styles.textInput}/>
+      <StatusBar style="light" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 10,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1a202c',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color:'#edf2f7',
+  },
+  textInput : {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor : '#2d3748',
+    marginVertical: 10,
+    width:'100%'
+  }
 });
---
+++ readme.md
@@ -0,0 +1,9 @@
+# instructions to run app
+
+```npm install expo-cli --global
+ cd mobile-client
+ yarn start # you can open iOS, Android, or web from here, or run them directly with the commands below.
+ yarn android
+ yarn ios
+ yarn web
+```
