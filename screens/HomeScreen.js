3ad2d18c819d6c4e105f7f6c143cc6db4c7dd50b
Pablo Lizardo <lizardo.pablo@gmail.com>
2020-08-20 16:59:58 -0300

    example project screen with api call

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,68 +1,75 @@
 import React, { useState, useContext } from "react";
-import { Text, View, SafeAreaView, StyleSheet  } from "react-native";
+import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 import AppContext from "../contexts/AppContext";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
-    const appContext = useContext(AppContext)
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text style={ styles.text }>Welcome {appContext.userdata.name || 'User'}</Text>
-      </SafeAreaView>
-    );
-  };
-  
+  const appContext = useContext(AppContext);
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.container,{paddingTop: insets.top,}]} >
+      <Text style={styles.title}>
+        Welcome {appContext.userdata.name || "User"}
+      </Text>
+    </View>
+  );
+};
+
 export default HomeScreen;
 
-
 const styles = StyleSheet.create({
-    form: {
-      flex: 1,
-      justifyContent: "center",
-    },
-    container: {
-      padding: 20,
-      flex: 1,
-      backgroundColor: "#1a202c",
-      alignItems: "center",
-      justifyContent: "center",
-      alignItems: "stretch",
-      justifyContent: "space-evenly",
-    },
-    text: {
-      color: "#a0aec0",
-      fontSize: 16,
-      textAlign: "center",
-      flex: 1,
-    },
-    button: {
-      paddingVertical: 15,
-      paddingHorizontal: 10,
-  
-      borderRadius: 10,
-      backgroundColor: "#e53e3e",
-      marginVertical: 10,
-    },
-    buttonText: {
-      textAlign: "center",
-      fontWeight: "600",
-      color: "#edf2f7",
-      fontSize: 24,
-    },
-    textInput: {
-      textAlign: "center",
-      paddingVertical: 15,
-      paddingHorizontal: 10,
-      color: "#edf2f7",
-      fontSize: 24,
-      borderRadius: 10,
-      backgroundColor: "#2d3748",
-      marginVertical: 5,
-    },
-    Logo: {
-      width: "100%",
-      height: 150,
-      resizeMode: "contain",
-      flex: 0.75,
-    },
-  });
-  
+  title: {
+    paddingVertical: 10,
+    textAlign: "left",
+    fontSize: 32,
+    fontWeight: "800",
+    color: "#e53e3e",
+    alignItems: "flex-start",
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  container: {
+    padding: 20,
+    backgroundColor: "#1a202c",
+    flex:1,
+  },
+  text: {
+    color: "#a0aec0",
+    fontSize: 16,
+    textAlign: "center",
+    flex: 1,
+  },
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+
+    borderRadius: 10,
+    backgroundColor: "#e53e3e",
+    marginVertical: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    fontWeight: "600",
+    color: "#edf2f7",
+    fontSize: 24,
+  },
+  textInput: {
+    textAlign: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    color: "#edf2f7",
+    fontSize: 24,
+    borderRadius: 10,
+    backgroundColor: "#2d3748",
+    marginVertical: 5,
+  },
+  Logo: {
+    width: "100%",
+    height: 150,
+    resizeMode: "contain",
+    flex: 0.75,
+  },
+});
--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -1,38 +1,75 @@
-import React, { useState, useContext } from "react";
-import { Text, View, SafeAreaView, StyleSheet  } from "react-native";
+import React, { useState, useContext, useEffect } from "react";
+import { Text, View, SafeAreaView, StyleSheet, ActivityIndicator  } from "react-native";
 import AppContext from "../contexts/AppContext";
+import configuration from "./../Configuration";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const ProjectsScreen = () => {
-    const appContext = useContext(AppContext)
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text style={ styles.text }>Lists of Projects</Text>
-      </SafeAreaView>
-    );
-  };
+  const insets = useSafeAreaInsets();
+
+  const appContext = useContext(AppContext)
+  const [ projects , setProjects ] = useState()
+  const fetchProjects = async () => {
+    const response = await fetch(`${configuration[appContext.env].api.baseUrl}/projects`, {
+      method: 'GET',
+      headers: { Authorization: 'Bearer ' + appContext.userdata.access_token }
+    })
+    const responseJSON = await response.json()
+    setProjects(responseJSON)
+  }
+  useEffect(() => { fetchProjects() }, [])
   
+  return (
+      <View style={[styles.container,{paddingTop: insets.top,}]} >
+
+      <Text style={ styles.title }>Projects</Text>
+      <View style={ styles.listWrapper}>
+      { projects ? projects.map( (project, index)=>{
+          return <TouchableOpacity key={index} style={ styles.listItem}>
+            <Text style={ styles.projectName }>{project.name}</Text>
+            <Text style={ styles.text }>{project.description}</Text>
+          </TouchableOpacity>
+        }) : <ActivityIndicator />}
+      </View>
+      </View>
+  );
+};
+
 export default ProjectsScreen;
 
 
 const styles = StyleSheet.create({
-    form: {
-      flex: 1,
-      justifyContent: "center",
+  title: {
+      paddingVertical:10, 
+      textAlign:'left',
+      fontSize: 32,
+      fontWeight:'800',
+      color:'#e53e3e',
+      alignItems:'flex-start'
+    },
+    listItem: {
+      paddingVertical:10, 
+      textAlign:'left',
+      alignItems:'flex-start'
+    },
+    listWrapper: {
+      flex:1,
     },
     container: {
-      padding: 20,
+      
+      paddingHorizontal: 20,
       flex: 1,
       backgroundColor: "#1a202c",
-      alignItems: "center",
-      justifyContent: "center",
-      alignItems: "stretch",
-      justifyContent: "space-evenly",
+    },
+    projectName: {
+      color: "#edf2f7",
+      fontSize: 20,
+      fontWeight:'700'
     },
     text: {
       color: "#a0aec0",
       fontSize: 16,
-      textAlign: "center",
-      flex: 1,
     },
     button: {
       paddingVertical: 15,
@@ -48,21 +85,6 @@ const styles = StyleSheet.create({
       color: "#edf2f7",
       fontSize: 24,
     },
-    textInput: {
-      textAlign: "center",
-      paddingVertical: 15,
-      paddingHorizontal: 10,
-      color: "#edf2f7",
-      fontSize: 24,
-      borderRadius: 10,
-      backgroundColor: "#2d3748",
-      marginVertical: 5,
-    },
-    Logo: {
-      width: "100%",
-      height: 150,
-      resizeMode: "contain",
-      flex: 0.75,
-    },
+    
   });
   
